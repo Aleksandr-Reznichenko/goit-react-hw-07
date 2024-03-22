f54494ff2c46baa4a7c032e51868b577f051a4c9
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-03-22 20:20:17 +0200

    Homework-07

--- src/components/ContactCount/ContactCount.jsx
+++ src/components/ContactCount/ContactCount.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contactsSlice';
+import { selectContactsItems } from '../../redux/contactsSlice';
 
 export default function ContactCount() {
-  const contactCount = useSelector(selectContacts).length;
+  const contactCount = useSelector(selectContactsItems).length;
 
   return <p>Contacts: ({contactCount})</p>;
 }
